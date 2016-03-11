f9147fc2304b0f1dfec001d60bbdaa905b9dc907
Alan Chen <alanc1995@gmail.com>
2016-03-11 02:06:52 -0500

    Added site

---
+++ scripts/projects.js
@@ -0,0 +1,28 @@
+$('.carousel').carousel();
+
+
+// var target = $("#navbar-relative").offset().top;
+//
+// intervalShow();
+//
+// function intervalShow() {
+// var interval = setInterval(function() {
+//       console.log("show");
+//       if ($(window).scrollTop() >= target) {
+//         $("#navbar-fixed").css({"visibility" : "visible"});
+//         clearInterval(interval);
+//         intervalHide();
+//       }
+//   }, 250);
+// }
+//
+// function intervalHide() {
+//   var interval = setInterval(function() {
+//     console.log("hide");
+//     if ($(window).scrollTop() <= target) {
+//       $("#navbar-fixed").css({"visibility" : "hidden"});
+//       clearInterval(interval);
+//       intervalShow();
+//     }
+//   }, 250);
+// }
